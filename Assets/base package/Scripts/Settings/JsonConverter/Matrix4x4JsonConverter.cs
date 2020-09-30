29a62f203dbd5f94ab69b556fe4cfe4bc04ee3a8
Шамин Александр Юрьевич <shamin@nkbvs.ru>
2020-09-30 16:40:41 +0300

    feat: Добавил JsonConverter
    JsonConverter для следующих объектов:
    - Color
    - Matrix4x4
    - Vector2Int
    - Vector2
    - Vector3
    
    Сериализация/десериализация данных объектов будет упрощена.

---
+++ Assets/base package/Scripts/Settings/JsonConverter/Matrix4x4JsonConverter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text;
+
+namespace Common
+{
+	public class Matrix4x4JsonConverter : JsonConverter<Matrix4x4>
+	{
+		public override Matrix4x4 ReadJson(Newtonsoft.Json.JsonReader reader, Type objectType, Matrix4x4 existingValue, bool hasExistingValue, JsonSerializer serializer)
+		{
+			if (reader.TokenType == JsonToken.StartObject)
+			{
+				JObject item = JObject.Load(reader);
+				StringBuilder str = new StringBuilder(5); 
+				for (int iy = 0; iy < 4; ++iy)
+				{
+					for (int ix = 0; ix < 4; ++ix)
+					{
+						str.Append($"m{iy}{ix}");
+						JToken value;
+						if(item.TryGetValue(str.ToString(), out value))
+						{
+							existingValue[iy, ix] = value.Value<float>();
+						}
+						str.Clear();
+					}
+				}
+			}
+			else
+			{
+				for (int iy = 0; iy < 4; ++iy)
+				{
+					for (int ix = 0; ix < 4; ++ix)
+					{
+						existingValue[iy, ix] = (float)reader.ReadAsDouble();
+					}
+				}
+			}
+
+			return existingValue;
+		}
+
+		public override void WriteJson(JsonWriter writer, Matrix4x4 value, JsonSerializer serializer)
+		{
+			writer.WriteStartObject();
+			for (int iy = 0; iy < 4; ++iy)
+			{
+				for (int ix = 0; ix < 4; ++ix)
+				{
+					writer.WritePropertyName($"m{iy}{ix}");
+					writer.WriteValue(value[iy, ix]);
+				}
+			}
+			writer.WriteEndObject();
+		}
+	}
+}
---
+++ Assets/base package/Scripts/Settings/JsonConverter/Vector2IntJsonConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Common
+{
+	public class Vector2IntJsonConverter : JsonConverter<Vector2Int>
+	{
+		public override Vector2Int ReadJson(Newtonsoft.Json.JsonReader reader, Type objectType, Vector2Int existingValue, bool hasExistingValue, JsonSerializer serializer)
+		{
+			if (reader.TokenType == JsonToken.StartObject)
+			{
+				JObject item = JObject.Load(reader);
+				existingValue.x = item["X"].Value<int>();
+				existingValue.y = item["Y"].Value<int>();
+			}
+			else
+			{
+				existingValue.x = reader.ReadAsInt32().Value;
+				existingValue.y = reader.ReadAsInt32().Value;
+			}
+			return existingValue;
+		}
+
+		public override void WriteJson(JsonWriter writer, Vector2Int value, JsonSerializer serializer)
+		{
+			writer.WriteStartObject();
+			writer.WritePropertyName("X");
+			writer.WriteValue(value.x);
+			writer.WritePropertyName("Y");
+			writer.WriteValue(value.y);
+			writer.WriteEndObject();
+		}
+	}
+}
