ade904d1569df58a2c39402c353270cfec28354a
Шамин Александр Юрьевич <shamin@nkbvs.ru>
2021-02-17 20:38:44 +0300

    feat: Добавил класс событий с дополнительным обработчиком

---
+++ Scripts/Events/EventWithAdditionalHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Common
+{
+	/// <summary>
+	/// Simple event with additional handler then invoke
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public class EventWithAdditionalHandler<T>
+	{
+		private event Action<T> _internalEvent;
+
+		public delegate void AdditionalHandlerDelegate(ref T value);
+		public AdditionalHandlerDelegate AdditionalHandler;
+
+		/// <summary>
+		/// Invoke inserted event
+		/// </summary>
+		/// <param name="data"></param>
+		public void InvokeEvent(T data)
+		{
+			AdditionalHandler?.Invoke(ref data);
+			_internalEvent?.Invoke(data);
+		}
+
+		/// <summary>
+		/// Overloading for easier access to events +=
+		/// </summary>
+		public static EventWithAdditionalHandler<T> operator +(EventWithAdditionalHandler<T> eventWithAdditionalHandler, Action<T> action)
+		{
+			eventWithAdditionalHandler._internalEvent += action;
+			return eventWithAdditionalHandler;
+		}
+
+		/// <summary>
+		/// Overloading for easier access to events -=
+		/// </summary>
+		public static EventWithAdditionalHandler<T> operator -(EventWithAdditionalHandler<T> eventWithAdditionalHandler, Action<T> action)
+		{
+			eventWithAdditionalHandler._internalEvent -= action;
+			return eventWithAdditionalHandler;
+		}
+	}
+}
