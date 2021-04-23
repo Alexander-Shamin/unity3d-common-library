1be52772490a9db63f884998660c698506eb6ca2
Шамин Александр Юрьевич <shamin@nkbvs.ru>
2021-04-23 15:07:58 +0300

    delete unused code

--- Scripts/InternalFunction.cs
+++ Scripts/InternalFunction.cs
@@ -9,97 +9,6 @@ namespace Common
 	/// </summary>
 	public class _
 	{
-
-		/// <summary>
-		/// Проверка флага и в случае false -> выход из программы
-		/// </summary>
-		public static void Require(bool flag, string str)
-		{
-			if (!flag)
-			{
-				Debug.LogError(str);
-#if UNITY_EDITOR
-				Debug.DebugBreak();
-#else
-				Application.Quit();
-#endif
-			}
-		}
-
-		public static void DebugErrorException(System.Exception exc, string str = "Exception! ")
-		{
-			Debug.LogError(str + exc.Message + exc.StackTrace);
-		}
-
-		static public void LogError(string message, MethodBase mb)
-		{
-			Debug.LogError($"{mb.Name} :: {mb.DeclaringType} :: {message}");
-		}
-		static public void LogError(string message)
-		{
-			Debug.LogError($"{message}");
-		}
-
-		static public void Log(string message, MethodBase mb)
-		{
-			Debug.Log($"{mb.Name} :: {mb.DeclaringType} :: {message}");
-		}
-		static public void Log(string message)
-		{
-			Debug.Log($"{message}");
-		}
-
-		static public void LogWarning(string message, MethodBase mb)
-		{
-			Debug.LogWarning($"{mb.Name} :: {mb.DeclaringType} :: {message}");
-		}
-
-		static public void LogWarning(string message)
-		{
-			Debug.LogWarning($"{message}");
-		}
-
-		static public void LogException(System.Exception exception, MethodBase mb)
-		{
-			Debug.LogError($"{mb.Name} :: {mb.DeclaringType} :: Exception \n {exception.Message} \n {exception.StackTrace}");
-		}
-
-		static public void LogException(System.Exception exception)
-		{
-			Debug.LogError($"Exception \n {exception.Message} \n {exception.StackTrace}");
-		}
-
-		/// <summary>
-		/// if object == null LogError(message, mb) => false
-		/// </summary>
-		/// <param name="obj"></param>
-		/// <param name="message"></param>
-		/// <param name="mb"></param>
-		static public bool CheckNull(object obj, string message, MethodBase mb)
-		{
-			if (obj == null)
-			{
-				_.LogError(message, mb);
-				return true;
-			}
-			return false;
-		}
-
-		/// <summary>
-		/// if object == null LogError(message)
-		/// </summary>
-		/// <param name="obj"></param>
-		/// <param name="message"></param>
-		static public bool CheckNull(object obj, string message)
-		{
-			if (obj == null)
-			{
-				_.LogError(message);
-				return true;
-			}
-			return false;
-		}
-
 		public static Texture2D TextureToTexture2D(Texture texture)
 		{
 			Texture2D texture2D = new Texture2D(texture.width, texture.height,
@@ -174,14 +83,5 @@ namespace Common
 				rawImage.uvRect = uvRect;
 			}
 		}
-
-
-		public static T GetComponentInChildred<T>(Component obj, string message, MethodBase mb)
-		{
-			var component = obj.GetComponentInChildren<T>();
-			CheckNull(component, message, mb);
-			return component;
-		}
-
 	} // class
 }
